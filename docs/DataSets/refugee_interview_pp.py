91aed0eae3f45d6bda334a36ec8bedff453f3f49
StanvanderBij <stan.van.der.bij@student.uva.nl>
2024-06-14 14:19:20 +0200

    interview_filter.2

--- docs/DataSets/refugee_interview_pp.py
+++ docs/DataSets/refugee_interview_pp.py
@@ -1,10 +1,9 @@
-
 import pandas as pd
 
 file_path = 'refugee_interview.csv'
 data = pd.read_csv(file_path)
 
-filtered_df = data[~data['VAR01'].isin([1, 2])]
+filtered_df = data[~data['VAR01'].isin([1, 2])] #1 and 2 are Iraq and Lebanon, these are not in Europe
 
 filtered_file_path = 'refugee_interview_filtered.csv'
 filtered_df.to_csv(filtered_file_path, index=False)
